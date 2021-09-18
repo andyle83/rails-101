46f102c5f27e65aab375e31d028f1badb0841f2f
Tuan Anh Le <anh.le@myob.com>
2021-09-18 23:34:32 +1000

    feat: model validation test

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,22 @@
-require "test_helper"
+require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'the truth' do
+    assert true
+  end
+
+  test 'should save an article with title and body' do
+    article = Article.new(title: 'title', body: 'content at least 10 characters')
+    assert article.save
+  end
+
+  test 'should not save article without title' do
+    article = Article.new
+    assert_not article.save
+  end
+
+  test 'should not save article with body less than 10 characters' do
+    article = Article.new(title: 'title', body: 'short')
+    assert_not article.save
+  end
 end
