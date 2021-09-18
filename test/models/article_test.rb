ef68426365974feb8d815999fcb9a63b102f80d1
Tuan Anh Le <anh.le@myob.com>
2021-09-18 23:36:45 +1000

    feat: test description

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -7,16 +7,16 @@ class ArticleTest < ActiveSupport::TestCase
 
   test 'should save an article with title and body' do
     article = Article.new(title: 'title', body: 'content at least 10 characters')
-    assert article.save
+    assert article.save, 'Save an article with title and content'
   end
 
   test 'should not save article without title' do
     article = Article.new
-    assert_not article.save
+    assert_not article.save, 'Unable to save article without title and body'
   end
 
   test 'should not save article with body less than 10 characters' do
     article = Article.new(title: 'title', body: 'short')
-    assert_not article.save
+    assert_not article.save, 'Unable to save article with body length less than 10'
   end
 end
